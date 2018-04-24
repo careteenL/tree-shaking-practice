a6f84f3f866c087f38900c4e6cf75e2024b232d2
careteenL <15074806497@163.com>
2018-04-24 18:10:01 +0800

    add vue-element and react-antd

---
+++ element-with-opt/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <el-button type="success" @click="handleClick">测试按钮</el-button>
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <HelloWorld/> -->
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  },
+  methods: {
+    handleClick () {
+      console.log('你敢点我')
+    }
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
