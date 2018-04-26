9060350e16d9c04de4d14760b5a01a23fb886016
careteenL <15074806497@163.com>
2018-04-26 21:47:51 +0800

    a

--- css-tree-shaking/webpack.config.js
+++ css-tree-shaking/webpack.config.js
@@ -1,11 +1,12 @@
 var webpack = require('webpack')
 var path = require('path')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+var CSSTreeShakingPlugin = require('webpack-css-treeshaking-plugin')
 
 module.exports = {
   // 入口文件地址，不需要写完，会自动查找
   entry: path.join(__dirname, 'src/main.js'),
-  output: {filename: 'bundle-use-opt.js'},
+  output: {filename: 'bundle.js'},
   module: {
     rules: [
       {
@@ -13,36 +14,37 @@ module.exports = {
         loaders: 'babel-loader'
       },
       {
-          test: /\.css$/,
-          loader: 'style-loader!css-loader'
+        test: /\.css$/,
+        loaders: 'style-loader!css-loader'
       },
       {
-          test: /\.less$/,
-          loader: 'style-loader!css-loader!less-loader'
+        test: /\.less$/,
+        loaders: 'style-loader!css-loader!less-loader'
       }
     ]
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: 'production'
-      }
-    }),
+    // new webpack.DefinePlugin({
+    //   'process.env': {
+    //     NODE_ENV: 'production'
+    //   }
+    // }),
+    new CSSTreeShakingPlugin(),
     new webpack.optimize.ModuleConcatenationPlugin(),
     new webpack.optimize.UglifyJsPlugin({
       beautify: false,
       comments: false,
       compress: {
         warnings: false,
-        drop_console: true,
         collapse_vars: true,
         reduce_vars: true
       }
     }),
     new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: path.join(__dirname, 'index.html'),
-        inject: true
+      filename: 'index.html',
+      template: path.join(__dirname, 'index.html'),
+      inject: true
     })
+    
   ]
 }
