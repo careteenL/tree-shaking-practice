dc546012bf2a85383d1c5fff21d6df6511e1a1dd
careteenL <15074806497@163.com>
2018-04-27 10:07:51 +0800

    add side effect

---
+++ rollup-webpack-cc/src/side-effect.js
@@ -0,0 +1,32 @@
+function Menu() {
+}
+
+Menu.prototype.show = function() {
+}
+
+Array.prototype.unique = function() {
+  // 将 array 中的重复元素去除
+}
+
+export default Menu;
+
+
+function Menu() {
+}
+
+Menu.prototype.show = function() {
+}
+
+var a = 'Arr' + 'ay'
+var b
+if(a == 'Array') {
+    b = Array
+} else {
+    b = Menu
+}
+
+b.prototype.unique = function() {
+    // 将 array 中的重复元素去除
+}
+
+export default Menu;
